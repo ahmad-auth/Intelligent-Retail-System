3646ac60bd217b68dff948cb4e3e0331d65de0fb
hasnatbashir <hasnatbashir@users.noreply.github.com>
2020-05-26 14:15:56 +0500

    Created reports modulle

---
+++ irs-client/src/app/reports/reports.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Routes, RouterModule } from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
+import { IrsapiService } from '../irsapi.service';
+
+import {MainModule} from '../main/main.module';
+
+const routes: Routes = [
+  // {path: 'inventory', component: ItemsComponent, outlet: 'dashboard-outlet'}
+];
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    FontAwesomeModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    MainModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    IrsapiService
+  ]
+})
+export class ReportsModule {
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+
+ }
