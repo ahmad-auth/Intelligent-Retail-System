098f41f143088c5cf7eef761c3775da69f303cf1
hasnatbashir <hasnatbashir900@gmail.com>
2020-06-18 21:42:56 +0500

    Added discard functionality to edit category form

--- irs-client/src/app/inventory/category/edit-category/discard-edit-category/discard-edit-category.component.ts
+++ irs-client/src/app/inventory/category/edit-category/discard-edit-category/discard-edit-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { DiscardCreateCategoryComponent } from '../../create-category/discard-create-category/discard-create-category.component';
 
 @Component({
   selector: 'app-discard-edit-category',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscardEditCategoryComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+    private dialog: MatDialog, private dialogRef: MatDialogRef<DiscardCreateCategoryComponent>) {} // Closing dialog window
+  ngOnInit(): void {}
+    
+  public cancel(): void { // To cancel the dialog window
+    this.dialogRef.close();
+  }
+  
+  public cancelN(): void { 
+    this.dialog.closeAll();
   }
 
 }
